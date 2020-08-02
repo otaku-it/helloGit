5d13b914f0589dd1d94ea5ae4f317e49387e48bc
tanrx <tanrixin@qjdchina.com>
2020-08-02 17:25:47 +0800

    dev更新--3

--- HellGit/src/main/java/com/tan/HelloGit.java
+++ HellGit/src/main/java/com/tan/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
         System.out.println("创建分支");
         System.out.println("分支添加");
         System.out.println("222");
+        System.out.println("dev---44");
     }
 }
--- HellGit/src/main/java/com/tan/Users.java
+++ HellGit/src/main/java/com/tan/Users.java
@@ -4,5 +4,6 @@ public class Users {
 
     public static void main(String[] args) {
         System.out.println("dev");
+        System.out.println("dev-222");
     }
 }
